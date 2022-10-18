9405e63e23717eb1464d16e2aed48179b38f7dbe
Cloudmersive <support@cloudmersive.com>
2022-10-17 20:53:42 -0700

    added

--- src/main/java/com/cloudmersive/Main.java
+++ src/main/java/com/cloudmersive/Main.java
@@ -1,10 +1,9 @@
 package com.cloudmersive;
 
-import com.cloudmersive.client.*;
-import com.cloudmersive.client.model.*;
-import com.cloudmersive.client.invoker.*;
-import com.cloudmersive.client.invoker.auth.*;
-import java.io.*;
+import com.cloudmersive.client.rt.invoker.*;
+import com.cloudmersive.client.rt.invoker.auth.*;
+import com.cloudmersive.client.rt.invoker.models.*;
+import com.cloudmersive.client.rt.*;
 
 public class Main {
 
@@ -15,28 +14,36 @@ public class Main {
 
         try
         {
-            // Customer
-
             ApiClient defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setBasePath("https://api.cloudmersive.com");
+        
+        // Configure API key authorization: Apikey
+        ApiKeyAuth Apikey = (ApiKeyAuth) defaultClient.getAuthentication("Apikey");
+        Apikey.setApiKey("YOUR API KEY");
+        // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+        //Apikey.setApiKeyPrefix("Token");
 
-            // Configure API key authorization: Apikey
-            ApiKeyAuth Apikey = (ApiKeyAuth) defaultClient.getAuthentication("Apikey");
-            Apikey.setApiKey("YOUR-KEY");
-
-            
-            ScanApi apiInstance = new ScanApi();
-            File inputFile = new File("C:\\Users\\Administrator\\Documents\\GitHub\\Cloudmersive-Java-VirusScan-Example\\pdf1.pdf"); // File | Input file to perform the operation on.
-            
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(new byte[100]);
-            
-            try 
-            {
-                VirusScanResult result = apiInstance.scanFile(inputStream);
-                System.out.println(result);
-            } catch (ApiException e) {
-                System.err.println("Exception when calling ScanApi#scanFile");
-                e.printStackTrace();
-            }
+        ScanApi apiInstance = new ScanApi(defaultClient);
+        File inputFile = new File("/path/to/file"); // File | Input file to perform the operation on.
+        Boolean allowExecutables = true; // Boolean | Set to false to block executable files (program code) from being allowed in the input file.  Default is false (recommended).
+        Boolean allowInvalidFiles = true; // Boolean | Set to false to block invalid files, such as a PDF file that is not really a valid PDF file, or a Word Document that is not a valid Word Document.  Default is false (recommended).
+        Boolean allowScripts = true; // Boolean | Set to false to block script files, such as a PHP files, Python scripts, and other malicious content or security threats that can be embedded in the file.  Set to true to allow these file types.  Default is false (recommended).
+        Boolean allowPasswordProtectedFiles = true; // Boolean | Set to false to block password protected and encrypted files, such as encrypted zip and rar files, and other files that seek to circumvent scanning through passwords.  Set to true to allow these file types.  Default is false (recommended).
+        Boolean allowMacros = true; // Boolean | Set to false to block macros and other threats embedded in document files, such as Word, Excel and PowerPoint embedded Macros, and other files that contain embedded content threats.  Set to true to allow these file types.  Default is false (recommended).
+        Boolean allowXmlExternalEntities = true; // Boolean | Set to false to block XML External Entities and other threats embedded in XML files, and other files that contain embedded content threats.  Set to true to allow these file types.  Default is false (recommended).
+        Boolean allowInsecureDeserialization = true; // Boolean | Set to false to block Insecure Deserialization and other threats embedded in JSON and other object serialization files, and other files that contain embedded content threats.  Set to true to allow these file types.  Default is false (recommended).
+        Boolean allowHtml = true; // Boolean | Set to false to block HTML input in the top level file; HTML can contain XSS, scripts, local file accesses and other threats.  Set to true to allow these file types.  Default is false (recommended) [for API keys created prior to the release of this feature default is true for backward compatability].
+        String restrictFileTypes = "restrictFileTypes_example"; // String | Specify a restricted set of file formats to allow as clean as a comma-separated list of file formats, such as .pdf,.docx,.png would allow only PDF, PNG and Word document files.  All files must pass content verification against this list of file formats, if they do not, then the result will be returned as CleanResult=false.  Set restrictFileTypes parameter to null or empty string to disable; default is disabled.
+        try {
+            VirusScanAdvancedResult result = apiInstance.scanFileAdvanced(inputFile, allowExecutables, allowInvalidFiles, allowScripts, allowPasswordProtectedFiles, allowMacros, allowXmlExternalEntities, allowInsecureDeserialization, allowHtml, restrictFileTypes);
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling ScanApi#scanFileAdvanced");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
 
 
         }
